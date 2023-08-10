db6f8d3411637175c051292fb61545b76812b4ca
Malte0621 <50240920+Malte0621@users.noreply.github.com>
2023-08-10 19:16:29 +0200

    Some random updates .-.

--- src/RBX.h
+++ src/RBX.h
@@ -7,10 +7,34 @@
 #include <string>
 #include <any>
 #include <vector>
+#include <functional>
+#include <thread>
 
 extern "C" {
 	const char* lua_call(const char* code) {
 		return ""; // Gets replaced from the lua side.
+	}
+}
+
+namespace RBX_Utils {
+	namespace string {
+		std::vector<std::string> split(std::string str, std::string delimiter) {
+			std::vector<std::string> strings;
+
+			size_t pos = 0;
+			std::string token;
+			while ((pos = str.find(delimiter)) != std::string::npos) {
+				token = str.substr(0, pos);
+
+				strings.push_back(token);
+
+				str.erase(0, pos + delimiter.length());
+			}
+
+			strings.push_back(str);
+
+			return strings;
+		}
 	}
 }
 
@@ -75,6 +99,33 @@ namespace RBX {
 			}
 		}
 
+		static Instance* FromDebugId(std::string debugId) {
+			return new Instance(debugId, nullptr, true);
+		}
+		
+		//void ConnectEvent(std::string name, std::function<void(std::vector<std::any>)> callback) {
+		std::function<void()> ConnectEvent(std::string name, std::function<void(std::vector<std::string>)> callback) {
+			
+			// Make a new thread
+			std::thread* t = new std::thread(([=]() {
+				std::string ret = lua_call((std::string("local cs = game:GetService(\"CollectionService\") local inst = cs:GetTagged(\"") + DebugID + "\")[1] if inst then local event = inst." + name + " local dID = \"__dID_\" .. game:GetService(\"HttpService\"):GenerateGUID(false) cs:AddTag(event, dID) if event then local ret = {} local rawRet = {event:Wait()} for i,v in ipairs(rawRet) do if typeof(v) == \"Instance\" then local dID2 = nil for i,v in ipairs(cs:GetTags(v)) if #v > 6 and v:sub(1,6) == \"__dID_\" then dID2 = v break end end if not dID2 then dID2 = \"__dID_\" .. game:GetService(\"HttpService\"):GenerateGUID(false) cs:AddTag(v, dID2) end table.insert(ret, dID2) else table.insert(ret, tostring(v)) end end return table.concat(ret, \"§\") end end").c_str());
+				callback(RBX_Utils::string::split(ret, "§"));
+			}));
+
+			t->detach();
+
+			// Return a disconnect function that kills the thread
+			return [=]() {
+				delete t;
+			};
+		}
+		
+		//std::vector<std::string> WaitEvent(std::string name) {
+		std::vector<std::string> WaitEvent(std::string name) {
+			std::string ret = lua_call((std::string("local cs = game:GetService(\"CollectionService\") local inst = cs:GetTagged(\"") + DebugID + "\")[1] if inst then local event = inst." + name + " local dID = \"__dID_\" .. game:GetService(\"HttpService\"):GenerateGUID(false) cs:AddTag(event, dID) if event then local ret = {} local rawRet = {event:Wait()} for i,v in ipairs(rawRet) do if typeof(v) == \"Instance\" then local dID2 = nil for i,v in ipairs(cs:GetTags(v)) if #v > 6 and v:sub(1,6) == \"__dID_\" then dID2 = v break end end if not dID2 then dID2 = \"__dID_\" .. game:GetService(\"HttpService\"):GenerateGUID(false) cs:AddTag(v, dID2) end table.insert(ret, dID2) else table.insert(ret, tostring(v)) end end return table.concat(ret, \"§\") end end").c_str());
+			return RBX_Utils::string::split(ret, "§");
+		}
+
 		void SetProperty(std::string name, std::any value) {
 			std::string valueString = "";
 			if (value.type() == typeid(std::string)) {
@@ -131,7 +182,7 @@ namespace RBX {
 			return lua_call((std::string("local cs = game:GetService(\"CollectionService\") local inst = cs:GetTagged(\"") + DebugID + "\")[1] if inst then local ret = inst." + name + " if type(ret) ~= \"string\" then ret = tostring(ret) end return ret end").c_str());	
 		}
 
-		std::any CallMethod(std::string name, std::vector<std::any> args) {
+		std::vector<std::any> CallMethod(std::string name, std::vector<std::any> args = {}) {
 			std::string argsString = "";
 			for (int i = 0; i < args.size(); i++) {
 				std::string valueString = "";
@@ -161,24 +212,57 @@ namespace RBX {
 					argsString += ", ";
 				}
 			}
-			std::string valueString = lua_call((std::string("local cs = game:GetService(\"CollectionService\") local inst = cs:GetTagged(\"") + DebugID + "\")[1] if inst then local ret = inst:" + name + "(" + argsString + ") if type(ret) == \"string\" then ret = '\"' .. ret .. '\"' else ret = tostring(ret) end return ret end end").c_str());
+			std::string valueString = lua_call((std::string("local cs = game:GetService(\"CollectionService\") local inst = cs:GetTagged(\"") + DebugID + "\")[1] if inst then local ret = inst:" + name + "(" + argsString + ") if typeof(ret) == \"table\" then local ret2 = {} for i,v in pairs(ret) do if typeof(v) == \"Instance\" then local dID2 = nil for i,v in ipairs(cs:GetTags(v)) if #v > 6 and v:sub(1,6) == \"__dID_\" then dID2 = v break end end if not dID2 then dID2 = \"__dID_\" .. game:GetService(\"HttpService\"):GenerateGUID(false) cs:AddTag(v, dID2) end table.insert(ret2, dID2) else table.insert(ret2, tostring(v)) end end return table.concat(ret2, \"§\") end if type(ret) ~= \"string\" then ret = tostring(ret) end return ret end end").c_str());
 			if (valueString == "nil") {
-				return nullptr;
+				return { nullptr };
 			}
 			else if (valueString == "true") {
-				return true;
+				return { true };
 			}
 			else if (valueString == "false") {
-				return false;
-			}
-			else if (valueString[0] == '"') {
-				return valueString.substr(1, valueString.length() - 2);
+				return { false };
 			}
 			else if (valueString.find('.') != std::string::npos) {
-				return std::stod(valueString);
+				return { std::stod(valueString) };
 			}
 			else {
-				return std::stoi(valueString);
+				if (!std::isdigit(valueString[0])) {
+					if (valueString.find("§") != std::string::npos) {
+						std::vector<std::string> values;
+						std::string currentString = "";
+						for (int i = 0; i < valueString.length(); i++) {
+							if (valueString[i] == '§') {
+								values.push_back(currentString);
+								currentString = "";
+							}
+							else {
+								currentString += valueString[i];
+							}
+						}
+						values.push_back(currentString);
+						std::vector<std::any> returnValues;
+						for (int i = 0; i < values.size(); i++) {
+							if (values[i] == "nil") {
+								returnValues.push_back(nullptr);
+							}
+							else if (values[i] == "true") {
+								returnValues.push_back(true);
+							}
+							else if (values[i] == "false") {
+								returnValues.push_back(false);
+							}
+							else if (values[i].find('.') != std::string::npos) {
+								returnValues.push_back(std::stod(values[i]));
+							}
+							else {
+								returnValues.push_back(std::stoi(values[i]));
+							}
+						}
+						return returnValues;
+					}
+					return { valueString };
+				}
+				return { std::stoi(valueString) };
 			}
 		}
 		
@@ -187,9 +271,5 @@ namespace RBX {
 		}
 	private:
 		std::string DebugID;
-
-		static Instance* FromDebugId(std::string debugId) {
-			return new Instance(debugId, nullptr, true);
-		}
 	};
 }
